ec835eb7f0e8890c8e4fdacad46555e251a02aa1
nguyentd1501 <nguyen.td1501@gmail.com>
2017-11-30 00:48:37 +0700

    update constants

---
+++ src/constant.js
@@ -0,0 +1,29 @@
+export const genres = [
+    {id: 0, name: 'Action'},
+    {id: 1, name: 'Adventure'},
+    {id: 2, name: 'Animation'},
+    {id: 3, name: 'Biography'},
+    {id: 4, name: 'Comedy'},
+    {id: 5, name: 'Crime'},
+    {id: 6, name: 'Documentary'},
+    {id: 7, name: 'Drama'},
+    {id: 8, name: 'Family'},
+    {id: 9, name: 'Fantasy'},
+    {id: 10, name: 'History'},
+    {id: 11, name: 'Horror'},
+    {id: 12, name: 'Music'},
+    {id: 13, name: 'Musical'},
+    {id: 14, name: 'Mystery'},
+    {id: 15, name: 'Romance'},
+    {id: 16, name: 'SciFi'},
+    {id: 17, name: 'Sport'},
+    {id: 18, name: 'Thriller'},
+    {id: 19, name: 'War'}
+]
+
+export const roles = [
+    {id: 1, name: 'Administrator'},
+    {id: 2, name: 'Manager'},
+    {id: 3, name: 'Cashier'},
+    {id: 4, name: 'User'}
+]
