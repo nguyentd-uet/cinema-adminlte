b989aef7e01216c94d7c62aed5925f21081b971b
nguyentd1501 <nguyen.td1501@gmail.com>
2017-11-30 00:49:08 +0700

    edit api

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,21 @@
 import axios from 'axios'
 import config from '../config'
+// import state from '../store/state'
 
+const baseURL = config.serverURI
+axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+axios.defaults.headers.post['Accept'] = 'application/json'
+axios.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest'
 export default {
-  request (method, uri, data = null) {
-    if (!method) {
-      console.error('API function call requires method argument')
-      return
-    }
-
-    if (!uri) {
-      console.error('API function call requires uri argument')
-      return
-    }
-
-    var url = config.serverURI + uri
-    return axios({ method, url, data },
-      {headers: {'Content-Type': 'application/json-patch+json',
-        'Accept': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'}})
-  }
+  login: baseURL + '/User/Login',
+  movie: baseURL + '/Movie/',
+  cinema: baseURL + '/Cinema/',
+  room: baseURL + '/Room/',
+  getUserList: baseURL + '/User/List',
+  register: baseURL + '/User/Register',
+  setRole: baseURL + '/User/SetRole',
+  user: baseURL + '/User/',
+  schedule: baseURL + '/Schedule/',
+  ticket: baseURL + '/Ticket/'
 }
